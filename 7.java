4fc5d90f9cffff2a19ed05eb7fc174083480f9b0
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-28 15:31:55 +0530

    Create 7.java

---
+++ 7.java
@@ -0,0 +1,28 @@
+// Pattern program 7
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    int n = 5;
+	    int x = 1;
+		for(int i=1;i<=n;i++){
+		    for(int j=1;j<=n;j++){
+		        System.out.printf("%2d",x);
+		        x+=2;
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+/*
+
+13579
+1113151719
+2123252729
+3133353739
+4143454749
+
+
+
+*/
