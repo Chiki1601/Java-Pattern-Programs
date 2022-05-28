2341c2257ad9e853fe1d5920f8bf5acf1ebba0a0
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-28 15:28:09 +0530

    Create 5.java

---
+++ 5.java
@@ -0,0 +1,22 @@
+// Pattern program 5
+public class Main
+{
+	public static void main(String[] args) {
+		for(int i=5;i>=1;i--){
+		    for(int j=5;j>=1;j--){
+		        System.out.print(j);
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+/*
+
+54321
+54321
+54321
+54321
+54321
+
+*/
