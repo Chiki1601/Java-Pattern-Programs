c704ec0079d83d69f3dffa74b6a9847cf43b3fdd
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-28 15:36:42 +0530

    Create 10.java

---
+++ 10.java
@@ -0,0 +1,26 @@
+// Pattern program 10
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    
+	   
+		for(int i=1;i<=5;i++){
+		    for(int j=1;j<=3;j++){
+		        System.out.print(j+""+i+"");
+		       
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+/*
+
+112131
+122232
+132333
+142434
+152535
+
+*/
