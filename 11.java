5acb26bca6fb235acf2f0bb0c36bdfc4444f30e2
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-28 15:37:47 +0530

    Create 11.java

---
+++ 11.java
@@ -0,0 +1,26 @@
+// Pattern program 11
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    
+	   
+		for(int i=1;i<=5;i++){
+		    for(int j=1;j<=3;j++){
+		        System.out.print(i+""+j+"");
+		       
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+/*
+
+111213
+212223
+313233
+414243
+515253
+
+*/
