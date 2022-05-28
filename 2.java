fb4eb698c6a6ab2cce8937d742a66bfcce967abf
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-28 15:24:59 +0530

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,23 @@
+// Pattern program 2
+public class Main
+{
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++){
+		    for(int j=1;j<=5;j++){
+		        System.out.print(i);
+		    }
+		    System.out.println();
+		}
+	}
+}
+/*
+
+
+11111
+22222
+33333
+44444
+55555
+
+
+*/
