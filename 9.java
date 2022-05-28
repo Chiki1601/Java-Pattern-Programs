37a7e82e8fcce9adab4ef1ca753837b1157de10d
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-28 15:34:38 +0530

    Create 9.java

---
+++ 9.java
@@ -0,0 +1,26 @@
+// Pattern program 9
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    int n = 5;
+	   
+		for(int i=1;i<=n;i++){
+		    for(int j=1;j<=n;j++){
+		        System.out.printf("%2d",(i*j));
+		       
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+/*
+
+12345
+246810
+3691215
+48121620
+510152025
+
+*/
